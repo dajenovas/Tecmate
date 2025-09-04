9c74ee48456979518289027e50f38b0a921a8183
Jenovas <dajenovas@gmail.com>
2025-09-04 18:24:23 +0530

    fifth commit

--- src/app/events/events.module.ts
+++ src/app/events/events.module.ts
@@ -6,12 +6,14 @@ import { EventsMainComponent } from './events-main/events-main.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { SharedModule } from "../shared/shared.module";
 import { TechnicalEventsComponent } from './technical-events/technical-events.component';
+import { NonTechnicalComponent } from './non-technical/non-technical.component';
 
 
 @NgModule({
   declarations: [
     EventsMainComponent,
     TechnicalEventsComponent,
+    NonTechnicalComponent,
   ],
   imports: [
     SharedModule,
